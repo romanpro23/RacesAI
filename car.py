d9c0028bc732093d53dbd903e758abf45a0e8b7e
Roman Protsenko <12345pro12@gmail.com>
2023-12-28 03:15:48 +0200

    Fix driving, add drifting

--- car.py
+++ car.py
@@ -5,6 +5,8 @@ import math
 
 class Car:
     speed: float
+    drift_speed: float
+    drift_max_speed: float
     height: int
     width: int
     acceleration: float
@@ -12,56 +14,107 @@ class Car:
     grip: float
     color: tuple
     angle: float
+    brakes: float
+    maneuverability: float
 
     x: float
     y: float
     dx: float
     dy: float
     da: float
+    ds: float
+    dfx: float
+    dfy: float
 
     body: shapes.Rectangle
 
-    def __init__(self, height, width, acceleration, max_speed, grip, color):
+    def __init__(self, height, width, acceleration, max_speed, grip, maneuverability, brakes, drift_control, color):
         self.grip = grip
         self.height = height
         self.width = width
         self.acceleration = acceleration
         self.max_speed = max_speed
         self.color = color
+        self.maneuverability = maneuverability
+        self.brakes = brakes
+        self.drift_max_speed = max_speed * (1 - drift_control)
 
-        self.x = 100
-        self.y = 100
+        self.x = 300
+        self.y = 384
         self.dx = 0
         self.dy = 0
         self.da = 0
+        self.ds = 0
         self.angle = 0
         self.speed = 0
+        self.drift_speed = 0
 
         self.body = shapes.Rectangle(x=self.x, y=self.y, width=self.width, height=self.height, color=self.color)
 
     def draw(self):
         self.body.draw()
 
+    def sign(self, numb):
+        return 0 if numb == 0 else abs(numb) / numb
+
     def move(self, ds):
-        self.speed += ds * self.acceleration
+        self.ds = ds
+        if self.sign(self.ds) != self.sign(self.speed):
+            self.ds *= self.brakes
+            self.drift_speed *= (1 - self.acceleration)
+
+        self.speed += self.ds * self.acceleration
+
+        if abs(self.speed) > self.max_speed:
+            self.speed = self.max_speed * self.sign(self.speed)
+
+    def stop(self):
+        self.ds = 0
+        self.speed *= (1 - self.acceleration)
+        self.drift_speed *= (1 - self.acceleration)
 
     def rot(self, da):
-        self.da = da
-        self.angle += da
+        if abs(self.speed) > 0:
+            self.da = da * self.maneuverability
+            self.angle -= self.da * self.sign(self.ds)
+
+        if self.sign(self.da) == self.sign(self.angle):
+            self.drift_speed += self.da * self.grip * abs(self.speed / self.max_speed)
+            if abs(self.drift_speed) >= self.drift_max_speed:
+                self.drift_speed = self.drift_max_speed * self.sign(self.drift_speed)
+        else:
+            self.drift_speed *= (1 - self.acceleration)
+
+        if abs(self.angle) >= 360:
+            self.angle -= 360 * self.sign(self.angle)
 
     def update(self):
-        cos = math.cos(self.angle)
-        sin = math.sin(self.angle)
-        self.dx = self.speed * sin
-        self.dy = self.speed * cos
+        if abs(self.speed) < self.grip:
+            self.ds = 0
+            self.speed = 0
 
-        tx = self.width * cos + self.height * sin
-        ty = -self.width * sin + self.height * cos
+        if abs(self.drift_speed) < self.grip:
+            self.drift_speed = 0
+
+        if self.ds == 0:
+            self.speed -= self.grip * self.sign(self.speed)
+
+        cos = math.cos(math.radians(self.angle))
+        cos90 = math.cos(math.radians(90 - self.angle))
+        sin = math.sin(math.radians(self.angle))
+        sin90 = math.sin(math.radians(90 - self.angle))
+
+        self.dx = self.speed * sin + sin90 * self.drift_speed
+        self.dy = self.speed * -cos + cos90 * self.drift_speed
+        print(self.drift_speed)
+
+        tx = self.width * cos - self.height * sin
+        ty = self.width * sin + self.height * cos
 
         self.x += self.dx
         self.y += self.dy
-        print(tx, ty)
-        self.body.x = self.x - tx + self.width
-        self.body.y = self.y - ty + self.height
+
+        self.body.x = self.x + (self.width - tx) * 0.5
+        self.body.y = self.y + (self.height - ty) * 0.5
 
         self.body.rotation = -self.angle
--- main.py
+++ main.py
@@ -3,80 +3,93 @@ from pyglet import shapes
 import math
 
 from car import Car
+from car2 import Car2
 
-window = pyglet.window.Window(width=400, height=400, caption='Races AI')
-key_pressed = False
-car = Car(50, 20, 0.05, 1, 2, (255, 0, 0))
+window = pyglet.window.Window(width=1024, height=786, caption='Races AI')
+car = Car(25, 10, 0.05, 5, 0.005, 2, 1.5, 0.5, (255, 0, 0))
+car2 = Car2(25, 10, 0.05, 5, 0.005, 2, 1.5, 0.5, (0, 255, 0))
 
-speed_pressed = False
-speed_dir = 0
-turn_pressed = False
-turn_dir = 0
+direction = {
+    "up": False,
+    "down": False,
+    "left": False,
+    "right": False,
+    "stop": False
+}
 
 
 def update(dt):
     global speed_dir
     global turn_dir
 
-    if turn_dir == 1:
-        car.rot(1)
-    elif turn_dir == -1:
-        car.rot(-1)
-
-    if speed_dir == 1:
+    if direction["stop"] or (direction["up"] and direction["down"]):
+        car.stop()
+        car2.stop()
+    elif direction["up"]:
         car.move(-1)
-    elif speed_dir == -1:
+        car2.move(-1)
+    elif direction["down"]:
         car.move(1)
+        car2.move(1)
+    else:
+        car.move(0)
+        car2.move(0)
+
+    if direction["left"] and direction["right"]:
+        car.rot(0)
+        car2.rot(0)
+    elif direction["left"]:
+        car.rot(1)
+        car2.rot(1)
+    elif direction["right"]:
+        car.rot(-1)
+        car2.rot(-1)
+    else:
+        car.rot(0)
+        car2.rot(0)
+
+    # car.rot(turn_dir)
+    # car.move(speed_dir)
 
     car.update()
+    car2.update()
+
 
 @window.event
 def on_draw():
     window.clear()
     car.draw()
+    car2.draw()
 
 
 @window.event
 def on_key_press(symbol, modifiers):
-    global speed_dir
-    global turn_dir
-    global speed_pressed
-    global turn_pressed
-
-    if not speed_pressed:
-        if symbol == pyglet.window.key.UP or symbol == pyglet.window.key.W:
-            speed_dir = 1
-        elif symbol == pyglet.window.key.DOWN or symbol == pyglet.window.key.S:
-            speed_dir = -1
-
-    if not turn_pressed:
-        if symbol == pyglet.window.key.LEFT or symbol == pyglet.window.key.A:
-            turn_dir = 1
-        elif symbol == pyglet.window.key.RIGHT or symbol == pyglet.window.key.D:
-            turn_dir = -1
+    if symbol == pyglet.window.key.UP or symbol == pyglet.window.key.W:
+        direction["up"] = True
+    elif symbol == pyglet.window.key.DOWN or symbol == pyglet.window.key.S:
+        direction["down"] = True
+    elif symbol == pyglet.window.key.LEFT or symbol == pyglet.window.key.A:
+        direction["left"] = True
+    elif symbol == pyglet.window.key.RIGHT or symbol == pyglet.window.key.D:
+        direction["right"] = True
+    elif symbol == pyglet.window.key.SPACE:
+        direction["stop"] = True
 
 
 @window.event
 def on_key_release(symbol, modifiers):
-    global speed_dir
-    global turn_dir
-    global speed_pressed
-    global turn_pressed
-
-    print((symbol == pyglet.window.key.UP or symbol == pyglet.window.key.W or
-            symbol == pyglet.window.key.DOWN or symbol == pyglet.window.key.S))
-    if (symbol == pyglet.window.key.UP or symbol == pyglet.window.key.W or
-            symbol == pyglet.window.key.DOWN or symbol == pyglet.window.key.S):
-        speed_dir = 0
-        speed_dir = False
-
-    if (symbol == pyglet.window.key.LEFT or symbol == pyglet.window.key.A or
-            symbol == pyglet.window.key.RIGHT or symbol == pyglet.window.key.D):
-        turn_dir = 0
-        turn_pressed = False
+    if symbol == pyglet.window.key.UP or symbol == pyglet.window.key.W:
+        direction["up"] = False
+    elif symbol == pyglet.window.key.DOWN or symbol == pyglet.window.key.S:
+        direction["down"] = False
+    elif symbol == pyglet.window.key.LEFT or symbol == pyglet.window.key.A:
+        direction["left"] = False
+    elif symbol == pyglet.window.key.RIGHT or symbol == pyglet.window.key.D:
+        direction["right"] = False
+    elif symbol == pyglet.window.key.SPACE:
+        direction["stop"] = False
 
 
-# Додати функцію для оновлення гри кожен кадр
+
 pyglet.clock.schedule_interval(update, 1 / 60)
-
 pyglet.app.run()
